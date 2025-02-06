0568b8c4e10b0482595b6e5d242dcb02a838919a
root <root@lazyprata.>
2025-02-06 07:19:31 -0500

    Format script

--- src/ServiceManager.sol
+++ src/ServiceManager.sol
@@ -2,14 +2,23 @@
 pragma solidity ^0.8.26;
 
 import {ISignatureUtils} from "eigenlayer-contracts/src/contracts/interfaces/ISignatureUtils.sol"; // methods for signing
-import {IAVSDirectory} from "eigenlayer-contracts/src/contracts/interfaces/IAVSDirectory.sol"; // manage operators  
+import {IAVSDirectory} from "eigenlayer-contracts/src/contracts/interfaces/IAVSDirectory.sol"; // manage operators
 import {ECDSA} from "solady/utils/ECDSA.sol"; // algo for signing
+import {console} from "forge-std/console.sol";
 
 contract ServiceManager {
     using ECDSA for bytes32;
 
     // attributes
     address public immutable avsDirectory; // manage operators
+    // uint32 public latestTaskNum;
+    // mapping(address => bool) public operatorRegistered;
+    // mapping(uint32 => bytes32) public allTasksHashes;
+    // mapping(address => mapping(uint32 => bytes)) public allTaskResponses
+
+    // events
+    // event NewTaskCreated(uint32 indexed taskIndex, Task task);
+    // event TaskResponded(uint32 indexed taskIndex, Task task, bool isSafe, address operator);
 
 
     struct Task {
@@ -21,9 +30,30 @@ contract ServiceManager {
         avsDirectory = _avsDirectory;
     }
 
+    // modifier
+    modifier onlyOperator() {
+        // ensure that only operators can use
+        console.log("hi");
+        _;
+    }
 
     // Registering operators
+    function registerOperatorToAVS() {
+        console.log("register op");
+    }
     // Deregistering operators
+    function deRegisterOperatorFromAVS() {
+        console.log("deregister op");
+    }
     // Create Task
-    // Respons to Task
+    // emit is to log events
+    // memory are for temporary variable
+
+
+    // Respond to Task - call agentKit our defi agent
+    // check task is valid and in contract
+    // check task has been responded to
+    // check that response has been signed by a valid operator
+
+
 }
